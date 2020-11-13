dec52a86c3fd65b842df9a9ced1f8be80edae035
Regis Ribeiro <regisgomesr@gmail.com>
2020-11-13 16:46:34 -0300

    Header / Footer

---
+++ src/components/UniqueOverlay/index.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import useWrapperScroll from "../Model/useWrapperScroll";
+
+import { useTransform } from "framer-motion";
+
+import { Container, Header, Logo, Burger, Footer } from "./styles";
+
+const UniqueOverlay: React.FC = () => {
+  const { scrollYProgress } = useWrapperScroll();
+
+  const opacity = useTransform(scrollYProgress, [0.9, 1], [0, 1]);
+
+  return (
+    <Container>
+      <Header>
+        <Logo />
+        <Burger />
+      </Header>
+
+      <Footer style={{ opacity }}>
+        <ul>
+          <li>
+            <a href="#">UI Clone</a>
+          </li>
+          <li>
+            <a href="#">made with ❤</a>
+          </li>
+          <li>
+            <a href="#">by Regis Ribeiro</a>
+          </li>
+        </ul>
+      </Footer>
+    </Container>
+  );
+};
+
+export default UniqueOverlay;
